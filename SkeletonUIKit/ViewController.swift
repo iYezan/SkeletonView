c1289ebf9567ee84f02470845040e244960cb612
Yezan Ahmed <hashir_hush@hotmail.co.uk>
2022-04-15 20:28:03 +0100

    Initial Commit

---
+++ SkeletonUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkeletonUIKit
+//
+//  Created by iYezan on 15/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
